8ed943732039dcd989291f7a0d48abee268b920a
kfortuin <k.fortuin@hotmail.com>
2016-04-24 15:40:23 +0200

    Doomla
    Added Check function to log in system

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,12 @@
 <?php 
 	require "includes/connect.php";
+	require "logic/index.logic.php";
 
 	// Fuction to get the username of the current user by running checks to make sure the user's name exist and to see wether or not the cookie's username and token are equal to that of the database. Also checks to see if the expiry from the cookie is bigger than the expiry from the database. 
 
 	function getAccessUsername() {
 		global $db;
-		if(($_COOKIE['user'] == " ") || ($_COOKIE['token']) == " "){
+		if(!isset($_COOKIE['user']) || !isset($_COOKIE['token'])){
 			header ("Location: login.php");
 		}
 
@@ -21,21 +22,22 @@
 				return $username;
 			}
 		}
-
-
-		
-
-
-
 	}
 
-	getAccessUsername();
+	
 
-	// if ($token >= 0) {
-	// 	header ("Location: home/admin.php");
-	// } else {
-	// 	header ("Location: login.php");
-	// }
+	function checkAccess() {
+		$username = getAccessUsername();
+
+		if($username == false) {
+			php::header("login.php");
+		} else {
+			setAccess($username);
+		}
+	}
+
+
+	getAccessUsername();
 
 	
  ?>
